ad99abee2d91100b4db9405b28c05718f7904573
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-10-11 21:59:44 +0300

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar')
 const path = require('path')
 const fs = require('fs/promises')
+const Jimp = require('jimp');
 
 const { SECRET_KEY } = process.env;
 const avatarsDir = path.join(__dirname, '../', 'public', 'avatars')
@@ -104,14 +105,19 @@ const updateAvatar = async (req, res, next) => {
         const { path: tempUpload, originalname } = req.file;
         const filename = `${_id}_${originalname}`;
         const resultUpload = path.join(avatarsDir, filename);
+
+        const image = await Jimp.read(tempUpload);
+        image.resize(250, 250).write(tempUpload);
     
         await fs.rename(tempUpload, resultUpload);
         const avatarURL = path.join('avatars', filename);
         await User.findByIdAndUpdate(_id, { avatarURL });
     
-        res.json({
-            avatarURL,
-        });
+        // res.json({
+        //     avatarURL,
+        // });
+        
+        res.status(200).json({ avatarURL: avatarURL });
 
     } catch (error) {
     res.status(401).json({
